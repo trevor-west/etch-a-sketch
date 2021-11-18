22e437c92725fe6744addabbc250b394212dc419
Trevor West <indestructiblelion@tutanota.com>
2021-11-17 17:17:01 -0800

    update index.html and script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const gridContainer = document.querySelector('.grid-container');
+const gridDiv = document.createElement('div');
+gridDiv.classList.toggle('grid-border');
+gridContainer.appendChild('gridDiv');
