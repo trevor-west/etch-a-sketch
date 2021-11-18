bc57b180b27a0e944890b080b005a1df204fc087
Trevor West <indestructiblelion@tutanota.com>
2021-11-18 15:00:09 -0800

    update allowing for centered etch a sketch prototype, on mouse hover divs change blue to black

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const gridContainer = document.querySelector('.grid-container');
-const gridDiv = document.createElement('div');
-gridDiv.classList.toggle('grid-border');
-gridContainer.appendChild('gridDiv');
+const gridContainer = document.querySelector('#grid-container');
+
+
+for (let i = 0; i < 16; i++) {
+  const gridDiv = document.createElement('div');
+  gridDiv.classList.add('grid-border');
+  gridContainer.appendChild(gridDiv);
+}
+
+const gridDivHover = document.querySelectorAll('.grid-border');
+gridDivHover.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+    div.classList.toggle('black');
+  });
+});
