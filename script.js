df87397255fdc1048d5dba4d7c67f83131ac2377
Trevor West <indestructiblelion@tutanota.com>
2021-11-18 16:21:58 -0800

    update script.js adding New Etch button that doesn't work (can't convert number into loop for grid)

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 const gridContainer = document.querySelector('#grid-container');
 
-
-for (let i = 0; i < 16; i++) {
+// 16 x 16 grid, so 256 divs
+for (let i = 0; i < 256; i++) {
   const gridDiv = document.createElement('div');
   gridDiv.classList.add('grid-border');
   gridContainer.appendChild(gridDiv);
 }
 
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+
+function newEtchCheck(newEtch) {
+  newEtch = prompt("What do you want the size of the new grid? (max 100)");
+  if (newEtch > 100) {
+    newEtch = prompt("Please enter a size of the grid below 100.")
+  } else if (newEtch <= 100) {
+    newEtch = newEtch * newEtch;
+    return newEtch;
+  }
+}
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+  gridDivHover.forEach((div) => {
+    div.classList.remove('black');
+  });
+  removeAllChildNodes(gridContainer);
+  for (let i = 0; i < newEtchCheck(); i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.classList.add('grid-border');
+    gridContainer.appendChild(gridDiv);
+  }
+});
+
 const gridDivHover = document.querySelectorAll('.grid-border');
+
 gridDivHover.forEach((div) => {
     div.addEventListener('mouseover', () => {
-    div.classList.toggle('black');
+    div.classList.add('black');
   });
 });
