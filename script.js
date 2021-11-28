169f80815a1a0c15023c0225dec81827d58bc66e
Trevor West <indestructiblelion@tutanota.com>
2021-11-28 12:29:33 -0800

    fixed typo grid-template-rows adding s, all works now

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-// define query selector for css class grid-box
-const gridBox = document.querySelector(".grid-box-16");
+// define query selector for css class grid-box;
+const gridBox = document.querySelector(".grid-box");
 
 // loop to create 16 x 16 (256) divs
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   div.classList.add('box-div');
   gridBox.appendChild(div);
+}
+
+// button code for reset with new grid number 100 or less
+const newGridButton = document.querySelector('#new-grid');
+
+newGridButton.addEventListener('click', () => {
+  window.location.reload();
+    });
+
+let newGridNumber = prompt("Please enter a new grid range (100 or less)");
+if (newGridNumber > 100) {
+  newGridNumber = prompt("Please enter a new grid range 100 or less.");
+} else {
+  newGridDivs = newGridNumber * newGridNumber;
+  console.log(newGridDivs);
+  for (let i = 0; i < newGridDivs; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box-div');
+    gridBox.setAttribute('style', `grid-template-columns: repeat(` + newGridNumber + `, 1fr); grid-template-rows: repeat(` + newGridNumber +`, 1fr);`);
+    gridBox.appendChild(div);
+  }
 }
 
 // add event listener for mouse hover to turn box div bkrg. color into black
