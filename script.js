12e790427331ecfa160366bfc92565802b28c486
Trevor West <indestructiblelion@tutanota.com>
2021-11-29 09:39:15 -0800

    not working test for reset grid

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 // define query selector for css class grid-box;
 const gridBox = document.querySelector('.grid-box');
 
-// loop to create 16 x 16 (256) divs
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.classList.add('box-div');
-  gridBox.appendChild(div);
-}
-
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
 // button code for reset with new grid number 100 or less
 const newGridButton = document.querySelector('#new-grid');
 
+// function for loop to create 16 x 16 (256) divs
+function sixteenLoop() {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box-div');
+    gridBox.appendChild(div);
+  }
+}
+
+sixteenLoop();
+
+function resetDivs() {
+  const divs = document.querySelectorAll('div');
+  const div = document.querySelector('div');
+  gridBox.forEach((divs) => {
+    gridBox.removeChild(div);
+  });
+}
+
 newGridButton.addEventListener('click', () => {
-  removeAllChildNodes(gridBox);
+  resetDivs();
   let newGridNumber = prompt('Please enter a new grid range (100 or less)');
   if (newGridNumber > 100) {
     newGridNumber = prompt('Please enter a new grid range 100 or less.');
   } else {
-    newGridDivs = newGridNumber * newGridNumber;
-    console.log(newGridDivs);
-    for (let i = 0; i < newGridDivs; i++) {
-      const div = document.createElement('div');
-      div.classList.add('box-div');
-      gridBox.setAttribute('style', `grid-template-columns: repeat(` + newGridNumber + `, 1fr); grid-template-rows: repeat(` + newGridNumber +`, 1fr);`);
-      gridBox.appendChild(div);
+    let newGridDivs = newGridNumber * newGridNumber;
+    function newGridLoop() {
+      for (let i = 0; i < newGridDivs; i++) {
+        const div = document.createElement('div');
+        div.classList.add('box-div');
+        gridBox.setAttribute('style', `grid-template-columns: repeat(` + newGridNumber + `, 1fr); grid-template-rows: repeat(` + newGridNumber +`, 1fr);`);
+        gridBox.appendChild(div);
+      }
     }
+    newGridLoop();
   }
 });
 
